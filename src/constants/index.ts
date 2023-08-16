d07a6f46423f862489bda5a9097803e135c3b517
= <=>
2023-08-16 11:09:13 +0100

    ...

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,4 @@ import { API_DEV_URL, API_PROD_URL } from "@env";
 
 export const API_BASE_URL =  __DEV__ ? `${API_DEV_URL}` : `${API_PROD_URL}`;
 
-export const PAYMENT_OPTIONS = ['card', 'ussd', 'bank']
+export const PAYMENT_OPTIONS: any = ['card', 'ussd', 'bank']
